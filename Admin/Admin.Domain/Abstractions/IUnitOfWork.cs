945b18d88f0705d7fd9098c2e981deb9cc7d15cd
sabajangidze <jangidzesaba@gmail.com>
2023-02-20 00:20:28 +0400

    Update unit of work

--- Admin/Admin.Domain/Abstractions/IUnitOfWork.cs
+++ Admin/Admin.Domain/Abstractions/IUnitOfWork.cs
@@ -6,6 +6,20 @@ public interface IUnitOfWork : IDisposable
 {
     ITransaction BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
 
+    void Add<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit;
+
+    Task AddAsync<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit;
+
+    void Update<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit;
+
+    void Remove<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit;
+
+    void Attach<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit;
+
+    IQueryable<T> Query<T>() where T : class, IEntity<Guid>, IEntityAudit;
+
+    Task<T> GetById<T>(Guid id) where T : class, IEntity<Guid>, IEntityAudit;
+
     void Commit();
 
     Task CommitAsync();
--- Admin/Admin.Infrastructure/Repositories/UnitOfWork.cs
+++ Admin/Admin.Infrastructure/Repositories/UnitOfWork.cs
@@ -19,15 +19,61 @@ public class UnitOfWork : IUnitOfWork
         return new DbTransaction(_context.Database.BeginTransaction());
     }
 
-    public void Commit()
+    public void Add<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit
     {
+        var set = _context.Set<T>();
+        set.Add(entity);
     }
 
-    public Task CommitAsync()
+    public async Task AddAsync<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit
     {
+        var set = _context.Set<T>();
+        await set.AddAsync(entity);
+    }
+
+    public void Attach<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit
+    {
+        var set = _context.Set<T>();
+        set.Attach(entity);
+    }
+
+    void IUnitOfWork.Remove<T>(T entity)
+    {
+        var set = _context.Set<T>();
+        set.Remove(entity);
+    }
+
+    public void Update<T>(T entity) where T : class, IEntity<Guid>, IEntityAudit
+    {
+        var set = _context.Set<T>();
+        set.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public IQueryable<T> Query<T>() where T : class, IEntity<Guid>, IEntityAudit
+    {
+        return _context.Set<T>();
+    }
+
+    public async Task<T> GetById<T>(Guid id) where T : class, IEntity<Guid>, IEntityAudit
+    {
+        var value = _context.Set<T>();
+        var entity = await value.FirstOrDefaultAsync(x => x.Id == id);
+        return entity;
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
     }
 
     public void Dispose()
     {
+        _context = null;
     }
 }
